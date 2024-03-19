f4646428e847f5729e1c4d8e80b68b781430f2ef
Alexis LOUBIERE <aloubier@student.42.fr>
2024-03-19 11:50:22 +0100

    Added player files

---
+++ src/player_move.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_move.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aloubier <aloubier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 18:42:54 by aloubier          #+#    #+#             */
+/*   Updated: 2024/03/11 13:35:50 by aloubier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3d.h"
+
+void	rotate_right(t_data *data);
+void	rotate_left(t_data *data);
+
+static void	move_forward(t_data *data)
+{
+	double	m_x;
+	double	m_y;
+	t_point	map;
+
+	m_x = data->player.pos_x + data->player.dir_x * 3 * MS;
+	m_y = data->player.pos_y + data->player.dir_y * 3 * MS;
+	if (m_x < 0.25 || m_x > data->m_data.height - 0.25
+		|| m_y < 0.25 || m_y > data->m_data.width - 0.25)
+		return ;
+	map.x = data->map[(int)m_x][(int)(data->player.pos_y)];
+	map.y = data->map[(int)(data->player.pos_x)][(int)m_y];
+	if (map.x == 0 || (!BONUS && map.x == 1) || (BONUS && map.x == e_o))
+		data->player.pos_x += data->player.dir_x * MS;
+	if (map.y == 0 || (!BONUS && map.y == 1) || (BONUS && map.y == e_o))
+		data->player.pos_y += data->player.dir_y * MS;
+}
+
+static void	move_backward(t_data *data)
+{
+	double	m_x;
+	double	m_y;
+	t_point	map;
+
+	m_x = data->player.pos_x - data->player.dir_x * 3 * MS;
+	m_y = data->player.pos_y - data->player.dir_y * 3 * MS;
+	if (m_x < 0.25 || m_x > data->m_data.height - 0.25
+		|| m_y < 0.25 || m_y > data->m_data.width - 0.25)
+		return ;
+	map.x = data->map[(int)m_x][(int)(data->player.pos_y)];
+	map.y = data->map[(int)(data->player.pos_x)][(int)m_y];
+	if (map.x == 0 || (!BONUS && map.x == 1) || (BONUS && map.x == e_o))
+		data->player.pos_x -= data->player.dir_x * MS;
+	if (map.y == 0 || (!BONUS && map.y == 1) || (BONUS && map.y == e_o))
+		data->player.pos_y -= data->player.dir_y * MS;
+}
+
+static void	move_right(t_data *data)
+{
+	double	m_x;
+	double	m_y;
+	t_point	map;
+
+	m_x = data->player.pos_x + data->plane.x * 3 * MS;
+	m_y = data->player.pos_y + data->plane.y * 3 * MS;
+	if (m_x < 0.25 || m_x > data->m_data.height - 0.25
+		|| m_y < 0.25 || m_y > data->m_data.width - 0.25)
+		return ;
+	map.x = data->map[(int)m_x][(int)(data->player.pos_y)];
+	map.y = data->map[(int)(data->player.pos_x)][(int)m_y];
+	if (map.x == 0 || (!BONUS && map.x == 1) || (BONUS && map.x == e_o))
+		data->player.pos_x += data->plane.x * MS;
+	if (map.y == 0 || (!BONUS && map.y == 1) || (BONUS && map.y == e_o))
+		data->player.pos_y += data->plane.y * MS;
+}
+
+static void	move_left(t_data *data)
+{
+	double	m_x;
+	double	m_y;
+	t_point	map;
+
+	m_x = data->player.pos_x - data->plane.x * 3 * MS;
+	m_y = data->player.pos_y - data->plane.y * 3 * MS;
+	if (m_x < 0.25 || m_x > data->m_data.height - 0.25
+		|| m_y < 0.25 || m_y > data->m_data.width - 0.25)
+		return ;
+	map.x = data->map[(int)m_x][(int)(data->player.pos_y)];
+	map.y = data->map[(int)(data->player.pos_x)][(int)m_y];
+	if (map.x == 0 || (!BONUS && map.x == 1) || (BONUS && map.x == e_o))
+		data->player.pos_x -= data->plane.x * MS;
+	if (map.y == 0 || (!BONUS && map.y == 1) || (BONUS && map.y == e_o))
+		data->player.pos_y -= data->plane.y * MS;
+}
+
+void	move_player(t_data *data)
+{
+	if (data->keys.w)
+		move_forward(data);
+	if (data->keys.s)
+		move_backward(data);
+	if (data->keys.a)
+		move_left(data);
+	if (data->keys.d)
+		move_right(data);
+	if (data->keys.l)
+		rotate_left(data);
+	if (data->keys.r)
+		rotate_right(data);
+	if (data->keys.l != 1)
+		data->keys.l = 0;
+	if (data->keys.r != 1)
+		data->keys.r = 0;
+}
---
+++ src/player_rotate.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_rotate.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aloubier <aloubier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 18:42:59 by aloubier          #+#    #+#             */
+/*   Updated: 2023/12/21 18:43:00 by aloubier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3d.h"
+
+void	rotate_right(t_data *data)
+{
+	float	old_plane;
+	float	old_dir;
+
+	old_dir = data->player.dir_x;
+	data->player.dir_x = data->player.dir_x * cos(-RS * data->keys.r)
+		- data->player.dir_y * sin(-RS * data->keys.r);
+	data->player.dir_y = old_dir * sin(-RS * data->keys.r)
+		+ data->player.dir_y * cos(-RS * data->keys.r);
+	old_plane = data->plane.x;
+	data->plane.x = data->plane.x * cos(-RS * data->keys.r)
+		- data->plane.y * sin(-RS * data->keys.r);
+	data->plane.y = old_plane * sin(-RS * data->keys.r)
+		+ data->plane.y * cos(-RS * data->keys.r);
+}
+
+void	rotate_left(t_data *data)
+{
+	float	old_plane;
+	float	old_dir;
+
+	old_dir = data->player.dir_x;
+	data->player.dir_x = data->player.dir_x * cos(data->keys.l * RS)
+		- data->player.dir_y * sin(data->keys.l * RS);
+	data->player.dir_y = old_dir * sin(data->keys.l * RS)
+		+ data->player.dir_y * cos(data->keys.l * RS);
+	old_plane = data->plane.x;
+	data->plane.x = data->plane.x * cos(data->keys.l * RS)
+		- data->plane.y * sin(data->keys.l * RS);
+	data->plane.y = old_plane * sin(data->keys.l * RS)
+		+ data->plane.y * cos(data->keys.l * RS);
+}
