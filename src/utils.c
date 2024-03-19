1ab877bfad7374a28309c98ed0cb1f4e0dbb8573
Alexis LOUBIERE <aloubier@student.42.fr>
2024-03-19 11:40:37 +0100

    added utils

---
+++ src/utils.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aloubier <aloubier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 18:43:46 by aloubier          #+#    #+#             */
+/*   Updated: 2023/12/21 18:43:47 by aloubier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3d.h"
+
+void	ft_swap(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+double	ft_lerp(double a, double b, double t)
+{
+	return (a + t * (b - a));
+}
+
+int	ft_is_in_set(char c, char *set)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+float	ray_length(t_coord a, t_coord b, float ang)
+{
+	(void)ang;
+	return (sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y)));
+}
+
+int	ft_isws(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n'
+		|| c == '\r' || c == '\v' || c == '\f')
+		return (1);
+	return (0);
+}
